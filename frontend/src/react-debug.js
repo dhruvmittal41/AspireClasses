79b3302d1dfeacfaa3b54b55ba246e9765ff4c6c
Dhruv <mittaldhruv41@gmail.com>
2025-12-18 20:23:00 +0530

    Login Fixes

---
+++ frontend/src/react-debug.js
@@ -0,0 +1,5 @@
+import * as ReactA from "react";
+import * as ReactB from "react/jsx-runtime";
+
+console.log("React A === React B ?", ReactA === ReactB);
+console.log("ReactA.useContext:", ReactA.useContext);
