a53770b42290f40b6081877c63292dbc50da511f
Dhruv <mittaldhruv41@gmail.com>
2025-12-31 13:20:32 +0530

    fixed exports

--- frontend/src/Pages/Home_Page/dashboard/DailyQuote.jsx
+++ frontend/src/Pages/Home_Page/dashboard/DailyQuote.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useMemo } from "react";
 export const DailyQuote = React.memo(() => {
   const quotes = useMemo(
     () => [
