6e744259509ed1fb8203941bf3c57311346c2c32
Dhruv <mittaldhruv41@gmail.com>
2025-12-31 16:54:48 +0530

    Cleaned code structure

--- frontend/src/app/routes.config.js
+++ frontend/src/app/routes.config.js
@@ -13,27 +13,27 @@ import { UpdateQuestions } from "../Pages/Admin_Page/UpdateQuestions";
 import CreateNewTest from "../Pages/Admin_Page/CreateNewTest";
 
 export const publicRoutes = [
-    { path: "/", element: LandingPage },
-    { path: "/login", element: LoginPage },
-    { path: "/register", element: Register },
-    { path: "/admin/login", element: AdminLogin },
+    { path: "/", element: <LandingPage /> },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/register", element: <Register /> },
+    { path: "/admin/login", element: <AdminLogin /> },
 ];
 
 export const privateRoutes = [
-    { path: "/home", element: HomePage },
-    { path: "/tests/:id", element: TestPage },
-    { path: "/details/bundle/:id", element: ProductDetailsPage },
-    { path: "/payment/bundle/:id", element: PaymentPage },
+    { path: "/home", element: <HomePage /> },
+    { path: "/tests/:id", element: <TestPage /> },
+    { path: "/details/bundle/:id", element: <ProductDetailsPage /> },
+    { path: "/payment/bundle/:id", element: <PaymentPage /> },
 ];
 
 export const adminRoutes = [
     {
         path: "/admin",
-        element: AdminDashboard,
+        element: <AdminDashboard />,
         children: [
-            { path: "assign-test", element: AssignTest },
-            { path: "update-questions", element: UpdateQuestions },
-            { path: "create-test", element: CreateNewTest },
+            { path: "assign-test", element: <AssignTest /> },
+            { path: "update-questions", element: <UpdateQuestions /> },
+            { path: "create-test", element: <CreateNewTest /> },
         ],
     },
 ];
