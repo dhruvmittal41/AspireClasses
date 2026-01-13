f0ac19967d38d09acd6cd66d6e0c46d4cd5a765b
Dhruv <mittaldhruv41@gmail.com>
2026-01-13 18:35:48 +0530

    fixes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,7 +6,7 @@ const protect = (req, res, next) => {
 
     if (
         req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer ")
+        req.headers.authorization.startsWith("Bearer")
     ) {
         token = req.headers.authorization.split(" ")[1];
     }
