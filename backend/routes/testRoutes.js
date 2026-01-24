386a4bfa2e4cf8b93056ecb3e4a46e5bef7f6afb
Dhruv <mittaldhruv41@gmail.com>
2026-01-24 23:16:25 +0530

    fix auth issues

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -23,7 +23,7 @@ router.get('/tests', getAllTests);
 router.get('/demo-tests', getDemoTests);
 router.get('/upcoming-tests', getUpcomingTests);
 router.get('/tests/:id', getTestById);
-router.get('/tests/:id/questions', protect, getTestQuestions);
+router.get('/tests/:id/questions', getTestQuestions);
 router.post('/tests/:id/submit', submitTest);
 router.put('/questions/:id', updateQuestion);
 router.put('/tests/:id', updateTest);
